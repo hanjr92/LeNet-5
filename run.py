fdb9c951f2a55f56e666836e41bc85534ee7782e
hanjr92 <jiaronghan@outlook.com>
2024-03-19 11:36:55 +0800

    update

--- run.py
+++ run.py
@@ -49,8 +49,7 @@ def train(epoch):
         loss_list.append(loss.detach().cpu().item())
         batch_list.append(i+1)
 
-        if i % 10 == 0:
-            print('Train - Epoch %d, Batch: %d, Loss: %f' % (epoch, i, loss.detach().cpu().item()))
+        print('Train - Loss: %f' % (loss.detach().cpu().item()))
 
         loss.backward()
         optimizer.step()
